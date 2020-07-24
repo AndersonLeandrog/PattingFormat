6c764e70ab3d5ee7e1f85e0c3fac0464ed14ea30
Anderson Leandro <65420034+AndersonLeandrog@users.noreply.github.com>
2020-07-24 17:59:55 -0300

    Add files via upload

---
+++ _x32/start_usr.cmd
@@ -0,0 +1,20 @@
+﻿@echo off
+
+::copia todos os arquivos dentro da pasta root/sta/
+  xcopy root/sta/*.* root
+
+::abre a pasta root/sta/
+  cd root/sta
+
+::executa a instalação do Net.Framework e do Winrar e volta uma pasta
+  ndp48-web.exe /q /norestart
+  wrar580br.exe /S
+  cd..
+
+::move todos os arquivos .exe para dentro da pasta sta, volta duas pastas
+  move *.exe sta
+  cd..
+  cd..
+
+::Executa o programa.
+xPattingNProject.exe
